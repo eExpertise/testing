29b26f7a349b28608f87b4c4cd357b03c2fe6d40
TrueAmbition001 <30793560+TrueAmbition001@users.noreply.github.com>
2018-04-10 17:55:59 +0200

    same as before

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    'mongodb://<Filiboii>':'<Geforce GTX 1080 ti>@ds155218.mlab.com:55218/eexpertise-test'
+    'mongodb://<Filiboii>':'<Geforce>@ds155218.mlab.com:55218/eexpertise-test'
 };
