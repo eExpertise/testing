6022e7f7eab805a715c33a0e3e0e90b2120f6ddf
TrueAmbition001 <30793560+TrueAmbition001@users.noreply.github.com>
2018-04-10 17:41:19 +0200

    changed db URI

--- config/database.js
+++ config/database.js
@@ -1,1 +1,3 @@
-module.exports = 'mongodb://<Filiboii>:<Geforce GTX 1080 ti>@ds155218.mlab.com:55218/eexpertise-test';
+module.exports = {
+    'mongodb://<Filiboii>':'<Geforce GTX 1080 ti>@ds155218.mlab.com:55218/eexpertise-test'
+};
